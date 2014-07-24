0265ea6560f8157bcb2b9f37cb719f6476e8b344
KevinVdV <kevin@mire.be>
2014-07-24 12:37:30 +0200

    [Task: 19603] Ensure that the license check works for Mirage 2

--- dspace-mirage2/src/main/webapp/Mirage2/Gruntfile.js
+++ dspace-mirage2/src/main/webapp/Mirage2/Gruntfile.js
@@ -1,3 +1,10 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 // Generated on 2013-11-09 using generator-webapp 0.4.3
 'use strict';
 
