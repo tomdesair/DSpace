7762791ef7034091d5fc97f2da649b163fb4cbe0
Jonas Van Goolen <jonas@atmire.com>
2015-09-30 12:13:38 +0200

    DS-2635 Configuration of sort order split into 2 parts
    Originally, one sortOrder configured in the discovery.xml spring file would be used.
    This had an impact on both the sidebar and view more pages
    This commit splits this sortOrder to make it possible to have different sortOrder for the view more page and sidebar facet
    
    This was an improvement based on the following Jira ticket
    https://jira.duraspace.org/browse/DS-2635

--- dspace-jspui/src/main/java/org/dspace/app/webui/discovery/DiscoverUtility.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/discovery/DiscoverUtility.java
@@ -568,7 +568,7 @@ public class DiscoverUtility
                             // filterquery
                             queryArgs.addFacetField(new DiscoverFacetField(
                                     facet.getIndexFieldName(), facet.getType(),
-                                    10, facet.getSortOrder()));
+                                    10, facet.getSortOrderSidebar()));
                         }
                         else
                         {
@@ -656,7 +656,7 @@ public class DiscoverUtility
                             .getIndexFieldName(),
                             DiscoveryConfigurationParameters.TYPE_TEXT,
                            limit, facet
-                                    .getSortOrder(), facetPage * facetLimit));
+                                    .getSortOrderSidebar(), facetPage * facetLimit));
                 }
             }
         }
